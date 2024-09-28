43a734e1358662ee5e252e23376ab72f66e2ae21
raghukummarapurugu <raghusanvi123@gmail.com>
2024-09-28 17:50:55 +0200

    Add everything from old repo

---
+++ script.js
@@ -0,0 +1,79 @@
+window.addEventListener('load', startPage);
+
+/**
+ * Page starter on load
+ */
+function startPage() {
+    console.log('So you are reading the log, huh?');
+    activateHamburger();
+
+}
+
+
+/**
+ * Dice roll on header
+ */
+function rollDice() {
+   // const image = document.createElement('img');
+    
+    const die = document.getElementById("die");
+    let d1 = Math.floor(Math.random() * 6) +1;
+    if (d1 == 1) {
+            die.innerHTML = '<img src="./imgs/dice/1.png" alt="" srcset="">';
+        //image.src = './imgs/dice/1.png';
+        }
+        if (d1 == 2) {
+            die.innerHTML = '<img src="./imgs/dice/2.png" alt="" srcset="">';
+          // image.src = './imgs/dice/2.png';
+        }
+        if (d1 == 3) {
+            die.innerHTML = '<img src="./imgs/dice/3.png" alt="" srcset="">';
+           // image.src = './imgs/dice/3.png';
+        }
+        if (d1 == 4) {
+            die.innerHTML = '<img src="./imgs/dice/4.png" alt="" srcset="">';
+        }
+        if (d1 == 5) {
+            die.innerHTML = '<img src="./imgs/dice/5.png" alt="" srcset="">';
+        }
+        if (d1 == 6) {
+            die.innerHTML = '<img src="./imgs/dice/6.png" alt="" srcset="">';
+        }
+       // die.innerHTML = ""
+      //  die.append(image)
+
+}
+
+/**
+ * Hamburger-menu
+ */
+function activateHamburger() {
+    const ham = document.querySelector("#hamButton");
+    ham.addEventListener('click', toggleHamburger);
+
+}
+
+function toggleHamburger() {
+
+    const navbar = document.querySelector(".navbar");
+
+    navbar.classList.toggle("showNav");
+    navbar.classList.toggle("closeNav");
+    console.log('kjasföa');
+
+}
+
+/**
+ * 
+ *
+
+function navEvents() {
+    const about = document.querySelector('#aboutMeButton');
+    about.addEventListener('click', toggleAboutMe);
+}
+
+function toggleAboutMe() {
+
+    const aboutMePage
+}
+*/
